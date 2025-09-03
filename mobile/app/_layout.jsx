f272de65796217e8c6fc72253fee63c0bd84e848
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-03 22:26:47 +0300

    Set up meal API

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -3,13 +3,14 @@ import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Slot } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { COLORS } from "../constants/colors";
+import SafeScreen from "@/components/SafeScreen";
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
+      <SafeScreen>
         <Slot />
-      </SafeAreaView>
+      </SafeScreen>
     </ClerkProvider>
   );
 }
