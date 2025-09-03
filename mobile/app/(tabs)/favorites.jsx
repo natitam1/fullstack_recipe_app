ca61f2a631370380ad57192963c62dffbe03989d
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-03 22:17:54 +0300

    Move search.jsx and favorites.jsx files to (tabs) and fix path issue

--- mobile/app/(tabs)/favorites.jsx
+++ mobile/app/(tabs)/favorites.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const index = () => {
+const favorites = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text>favorites</Text>
     </View>
   );
 };
 
-export default index;
+export default favorites;
