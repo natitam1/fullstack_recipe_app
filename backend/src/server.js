6d8659388935eaf32b1a6b15af5aaccac72e5efa
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-03 06:07:58 +0300

    Set up database and create schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,13 @@
 import express from "express";
-import "dotenv/config";
+import { ENV } from "./config/env.js";
 
 const app = express();
 
-const PORT = process.env.PORT || 8001;
+const PORT = ENV.PORT || 8001;
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: true });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running at PORT:`, PORT);
+  console.log(`Server is running at localhost:${ENV.PORT}`);
 });
