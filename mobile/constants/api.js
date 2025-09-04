5e33b4721b4a1634f0c83ec1c07810ae75e1ff7a
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-04 13:43:01 +0300

    Update the api route

---
+++ mobile/constants/api.js
@@ -0,0 +1,3 @@
+// if you're using your physical phone, change this to the deployed url
+// we have explained this in the course :-)
+export const API_URL = "https://recipe-app-api-1-4ykb.onrender.com/api";
